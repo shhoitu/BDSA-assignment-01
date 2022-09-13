b9deeff78f1549dc461a6282c7ce5d4f4e784993
Sigurd Holm <sigurd@tiimo.dk>
2022-09-13 13:41:34 +0200

    Adds implementation

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -12,5 +12,10 @@ public static class Iterators
         }
     }
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) {
+        foreach (var item in items) {
+            if (!predicate(item)) continue;
+            yield return item;
+        }
+    }
 }
