b71a757f6a7c6dbc1afabbd938462dfd42d2c6da
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-13 13:04:36 +0200

    test is build, not passsed

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -2,4 +2,16 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void Helloworld_split_in_two(){
+        //Arrange
+        string[] hw = {"Hello World", "Mikkel og Clara"};
+
+        //Act
+        var split = RegExpr.SplitLine(hw);
+
+        //Assert
+        string[] excpect =  {"Hello","World", "Mikkel", "og", "Clara"} ;
+        Assert.Equal(excpect , split );
+    }   
 }
