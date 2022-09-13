d2e29b6bb8eb9ec9725dcd62701fdeafc88f53bb
Mikkel Bistrup Andersen <mbia@itu.dk>
2022-09-13 13:48:35 +0200

    started test

---
+++ Assignment1.Tests/ResolutionsTests.cs
@@ -0,0 +1,17 @@
+namespace Assignment1.Tests;
+
+public class ResolutionsTests
+{
+    [Fact]	
+    public void StringToTuples() {
+		// Arrange
+		//var string = 
+
+		// Act
+		
+		
+		// Assert
+        (int, int)[] except =  new (int, int)[]{(1024, 768), (800, 600), (640, 480)};
+		//Assert.Equal((new[] {0, 1, 5, 8, 2}).ToList(), result);
+	}
+}
