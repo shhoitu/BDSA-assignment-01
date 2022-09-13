70b190a868b1a9fa34afb13f8d892ba26e50a112
Sigurd Holm <sigurd@tiimo.dk>
2022-09-13 13:06:45 +0200

    Adds implementation and additional test cases

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,7 +2,15 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) {
+        foreach (var item in items)
+        {
+            foreach (var content in item) 
+            {
+                yield return content;
+            }
+        }
+    }
 
     public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
 }
