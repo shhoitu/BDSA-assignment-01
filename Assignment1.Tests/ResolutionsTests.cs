1dfba7a0ac309f03e72519a06acd7f85361abeac
Mikkel Bistrup Andersen <mbia@itu.dk>
2022-09-14 12:27:54 +0200

    Added resolutions test

--- Assignment1.Tests/ResolutionsTests.cs
+++ Assignment1.Tests/ResolutionsTests.cs
@@ -2,16 +2,17 @@ namespace Assignment1.Tests;
 
 public class ResolutionsTests
 {
-    [Fact]	
-    public void StringToTuples() {
-		// Arrange
-		//var string = 
+    [Fact]
+    public void StringToTuples()
+    {
+        // Arrange
+        string[] input = { "1920x1080", "1024x768, 800x600, 640x480" };
 
-		// Act
-		
-		
-		// Assert
-        (int, int)[] except =  new (int, int)[]{(1024, 768), (800, 600), (640, 480)};
-		//Assert.Equal((new[] {0, 1, 5, 8, 2}).ToList(), result);
-	}
+        // Act
+        var result = RegExpr.Resolution(input);
+
+        // Assert
+        (int, int)[] except = new (int, int)[] { (1920, 1080), (1024, 768), (800, 600), (640, 480) };
+        Assert.Equal(except, result);
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -4,7 +4,7 @@ public static class RegExpr
 {
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) => throw new NotImplementedException();
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
