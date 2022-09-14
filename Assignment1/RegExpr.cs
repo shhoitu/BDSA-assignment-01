b7429e4149d0ec71943a34cb138b78f4831b6cea
Mikkel Bistrup Andersen <mbia@itu.dk>
2022-09-14 12:48:23 +0200

    Began implementing resolution method

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,18 @@
 namespace Assignment1;
+using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
 
-    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
+    {
+        foreach (string str in resolutions)
+        {
+            string[] temp = Regex.Split(str, @"\,\x");
+            
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
