5ad0926e414e24dd37fb3c1c1f361d2501fff403
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-14 22:11:18 +0200

    with comments

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -9,13 +9,18 @@ public static class RegExpr
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines) 
     {
         var regex = "[^0-9a-zA-Z]+";
+        // The regex string
+
         foreach (string line in lines)
          {
             var split = Regex.Split(line, regex);
+            //Use Regex.Split to split the line where the regex fits
             
                  foreach (string regexLine in split)
                 {
                     yield return regexLine;
+                    //returns the individual words
+                    //yield statement used to return each element one at a time
                 }
             
 
@@ -23,18 +28,24 @@ public static class RegExpr
        
     }
     
-
     public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) 
     {
         var regex = @"(?<width>\d+)x(?<height>\d+)";
             foreach(string line in resolutions)
             {
-
                 foreach(Match number in Regex.Matches(line, regex))
                 {
+                    //Regex.Matches returns a collection of match objects that are found 
+                    //when comparing a line to the regex expression
+
                     GroupCollection groups = number.Groups;
+                    //Returns the set of captured groups in a single match.
+                    //So the first part of an expression has the value width, the second the value height
+
+                    int w = int.Parse(groups["width"].Value);
+                    int h = int.Parse(groups["height"].Value);
                 
-                    yield return (int.Parse(groups["width"].Value), int.Parse(groups["height"].Value));
+                    yield return (w , h);
                 }
             }
     }
@@ -42,11 +53,13 @@ public static class RegExpr
     public static IEnumerable<string> InnerText(string html, string tag) 
     {
         var regex = string.Format(@"<([{0}][^>]*)>(?<innerText>.+?)</{0}>", tag);
+        //The format inserts the tag in the regex expression where it says {0}
 
         foreach (Match match in Regex.Matches(html, regex))
         {
             GroupCollection groups = match.Groups;
             yield return Regex.Replace(groups["innerText"].Value, "<[^>]*>", "");
+            //replaces <[^>]*> with nothing ("")
         }
     }
 
@@ -56,14 +69,15 @@ public static class RegExpr
 
         foreach (Match match in Regex.Matches(html, regex))
         {
-            if(match.Success)
-            {
+            
                Uri url = new Uri(match.Groups["url"].Value);
-                string title = match.Groups["title"].Value;
+               //makes the uri
+
+                var title = match.Groups["title"].Value;
+                var innerText = match.Groups["innerText"].Value;
             
                 if (title != "") yield return (url, title);
-                else yield return ((url, match.Groups["innerText"].Value));
-            }
+                else yield return ((url, innerText));
             
         }
     }
